192af2cfcad4952a6c7ac05b450d4b9ad0a662ed
razgulchik <razgulchik@gmail.com>
2018-08-23 22:53:20 +0300

    ok

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float paddleLeft = 1f;
+    [SerializeField] float paddleRight = 15f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, paddleLeft, paddleRight);
+        transform.position = paddlePos;
+	}
+}
---
+++ Assets/Scripts/ScreenLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreenLoader : MonoBehaviour {
+
+    public void NextSceenLoader()
+    {
+        int sceenIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(sceenIndex + 1);
+    }
+	
+    public void LoadStartSceen()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void GameQuit()
+    {
+        Application.Quit();
+    }
+
+}
